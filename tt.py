7c23d356ff8b19f6090af00590f911c13f340ab2
Karl Fogel <kfogel@red-bean.com>
2023-01-29 21:54:51 -0600

    Make a basic loop from 1 to 10

--- tt.py
+++ tt.py
@@ -1,2 +1,11 @@
 
 print("Hello, World!")
+
+# Now let's print the numbers from 1 to 10.
+
+i = 1
+while (i <= 10):
+    print(f"This is the number {i}.")
+    i = i + 1
+
+print("That's all, folks!")
