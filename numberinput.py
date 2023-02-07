cffcf203937d4a6241a2b0d868aeaf778ef8fab9
TristanAMurray <122760620+TristanAMurray@users.noreply.github.com>
2023-02-06 22:55:08 -0600

    commit

---
+++ numberinput.py
@@ -0,0 +1,6 @@
+fnumber = input("What is your number?")
+num1 = fnumber
+fnumber2 = input("What is your second number?")
+num2 = fnumber2
+sum = num1 + num2
+print("Sum of {0} and {1} is {2}" .format(num1, num2, sum))
