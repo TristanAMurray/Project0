297e85159aaa4811c2b97a92fbf7a367619a3617
TristanAMurray <122760620+TristanAMurray@users.noreply.github.com>
2023-02-07 21:32:49 -0600

    fixing the code for the number input

--- numberinput.py
+++ numberinput.py
@@ -1,14 +1,13 @@
 
 prompt_1 = "What is your number?"
 fnumber = input(prompt_1)
-num1 = fnumber
+num1 = int(fnumber)
 
 prompt_2 = "What is your second number?"
 fnumber2 = input(prompt_2)
-num2 = fnumber2
+num2 = int(fnumber2)
 
 sum = num1 + num2
 print("Sum of {0} and {1} is {2}" .format(num1, num2, sum))
 
-together_string = prompt_1 + "  <yay>     " + prompt_2
-print(f"{together_string}")
+
