a52ae5d1c12fe0c89a592cff6ddf256a282e91cc
Karl Fogel <kfogel@red-bean.com>
2023-02-07 21:42:03 -0600

    More conditional exercises

--- numberinput.py
+++ numberinput.py
@@ -12,5 +12,13 @@ print("Sum of {0} and {1} is {2}" .format(num1, num2, sum))
 
 if True:
     print("This is what happens in the 'true' case.")
+    if 3 == int("3"):
+        print("Yep, int() behaves as expected.")
+    elif 3 == int("4"):
+        print("This line should never be printed.")
+    elif 3 == int("3"):
+        print("Interestingly, this line shouldn't be printed either.  Why not?")
+    else:
+        print("This line will never be printed either!  Sheesh.")
 else:
-    print("This is what happens in the 'false' (non-true) case.")
+    print("This would be what happens in the 'false' (non-true) case.")
