758ef3fb6c223725e2eb3de7c2f31f90c5c26e4d
Karl Fogel <kfogel@red-bean.com>
2023-02-07 22:05:46 -0600

    Start experimenting with tkinter

--- numberinput.py
+++ numberinput.py
@@ -1,24 +1,15 @@
+import tkinter
 
 prompt_1 = "What is your number?"
-fnumber = input(prompt_1)
-num1 = int(fnumber)
-
 prompt_2 = "What is your second number?"
-fnumber2 = input(prompt_2)
-num2 = int(fnumber2)
 
-sum = num1 + num2
-print("Sum of {0} and {1} is {2}" .format(num1, num2, sum))
+t = tkinter.Tk()
 
-if True:
-    print("This is what happens in the 'true' case.")
-    if 3 == int("3"):
-        print("Yep, int() behaves as expected.")
-    elif 3 == int("4"):
-        print("This line should never be printed.")
-    elif 3 == int("3"):
-        print("Interestingly, this line shouldn't be printed either.  Why not?")
-    else:
-        print("This line will never be printed either!  Sheesh.")
-else:
-    print("This would be what happens in the 'false' (non-true) case.")
+frame = tkinter.Frame(t, borderwidth=5)
+frame.pack(fill=tkinter.BOTH, expand=1)
+label = tkinter.Label(frame, text="Button Example")
+label.pack(fill=tkinter.X, expand=1)
+
+button = tkinter.Button(frame, text="Exit", command=t.destroy)
+button.pack(side=tkinter.BOTTOM)
+t.mainloop()
