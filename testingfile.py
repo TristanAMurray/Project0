76022cb520d764638844b61a278ac9fe23537c62
TristanAMurray <122760620+TristanAMurray@users.noreply.github.com>
2023-02-22 20:47:12 -0600

    My attempts to solve the problem

---
+++ testingfile.py
@@ -0,0 +1,10 @@
+
+prompt_1 = "What is your number?"
+fnumber = input(prompt_1)
+num1 = int(fnumber)
+
+prompt_2 = "What is your second number?"
+fnumber2 = input(prompt_2)
+num2 = int(fnumber2)
+
+print (fnumber-fnumber2)
