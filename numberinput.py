2bb022932b3c7b26b202a7be1d9cc1ecd531fb27
Karl Fogel <kfogel@red-bean.com>
2023-02-23 17:28:30 -0600

    Put the input and conversion code into a function
    This follows up to commit 320aea5038210a.

--- numberinput.py
+++ numberinput.py
@@ -1,39 +1,18 @@
 import sys
-def check(number):
+
+def get_integer_input(prompt):
+    """Return an integer from the user, prompting with PROMPT (a string).
+    If the user enters something other than an integer, throw an
+    exception."""
+    input_str = input(prompt)
     try:
-        number = int(fnumber2)
+        num = int(input_str)
     except:
         print("ERROR: I need a number, nothing else.")
-    sys.exit(1)
+    return num
 
-
-prompt_1 = "What is your number? "
-fnumber = input(prompt_1)
-
-num1 = int(fnumber)
-
-a = check(num1)
-
-print (a)
-
-# try:
-#     num1 = int(fnumber)
-# except: 
-#     print("ERROR: I need a number, nothing else.")
-#     sys.exit(1)
-
-prompt_2 = "What is your second number? "
-fnumber2 = input(prompt_2)
-num2 = int(fnumber2)
-
-a = check(num2)
-
-print(a)
-# try:
-#     num2 = int(fnumber2)
-# except:
-#     print("ERROR: I need a number, nothing else.")
-#     sys.exit(1)
+num1 = get_integer_input("What is your number? ")
+num2 = get_integer_input("What is your second number? ")
 
 if num1 > num2:
     tmp = num1
@@ -47,18 +26,21 @@ while i <= num2:
     sum = sum + i 
     i = i + 1
 
-
 print("Sum of {0} through {1} is {2}" .format(num1, num2, sum))
 
-if True:
-    print("This is what happens in the 'true' case.")
-    if 3 == int("3"):
-        print("Yep, int() behaves as expected.")
-    elif 3 == int("4"):
-        print("This line should never be printed.")
-    elif 3 == int("3"):
-        print("Interestingly, this line shouldn't be printed either.  Why not?")
-    else:
-        print("This line will never be printed either!  Sheesh.")
-else:
-    print("This would be what happens in the 'false' (non-true) case.")
+### This block was just for experimenting with if/else statements.
+### It doesn't serve any purpose in the program, so I've commented
+### it out for now.  You can remove it when you're ready.  -Karl
+#
+# if True:
+#     print("This is what happens in the 'true' case.")
+#     if 3 == int("3"):
+#         print("Yep, int() behaves as expected.")
+#     elif 3 == int("4"):
+#         print("This line should never be printed.")
+#     elif 3 == int("3"):
+#         print("Interestingly, this line shouldn't be printed either.  Why not?")
+#     else:
+#         print("This line will never be printed either!  Sheesh.")
+# else:
+#     print("This would be what happens in the 'false' (non-true) case.")
