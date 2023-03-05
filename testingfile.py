1eb05807b5f68d1bf9dcb1312be2b120113c0696
TristanAMurray <122760620+TristanAMurray@users.noreply.github.com>
2023-03-05 14:14:54 -0600

    Attempts to print a tic tac toe board.

--- testingfile.py
+++ testingfile.py
@@ -1,26 +1,10 @@
+import sys
 
-# prompt_1 = "What is your number?"
-# fnumber = input(prompt_1)
-# num1 = int(fnumber)
+def print_ttt_board():
+    print ("   |   |  ")
+    print ("-----------")
+    print ("   |   |  ")
+    print ("-----------")
+    print ("   |   |  ")
 
-# prompt_2 = "What is your second number?"
-# fnumber2 = input(prompt_2)
-# num2 = int(fnumber2)
-
-# print (fnumber-fnumber2)
-
-
-def function1():
-    print("WOWWWWW")
-    print("ALSO WOOAAAAHHHH")
-print("this is outside the funtion")
-
-function1()
-function1()
-
-def function2(x):
-    return 2*x
-
-a = function2(3)
-
-print (a)
+print_ttt_board()
