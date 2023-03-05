c443539b6c45535dc241379105796876edb31b5a
Karl Fogel <kfogel@red-bean.com>
2023-03-05 14:52:52 -0600

    Use sys.stdout for all output
    This avoids uneven / unpredictable stream interleaving.

--- testingfile.py
+++ testingfile.py
@@ -6,11 +6,11 @@ def print_ttt_board(x, y):
     j = 0
     while i < x:
         # The code inside these loops isn't quite correct yet.
-        sys.stderr.write(f"DEBUG: i is {i}\n")
-        sys.stderr.flush()
+        sys.stdout.write(f"DEBUG: i is {i}\n")
+        sys.stdout.flush()
         while j < y:
-            sys.stderr.write(f"DEBUG: j is {j}\n")
-            sys.stderr.flush()
+            sys.stdout.write(f"DEBUG: j is {j}\n")
+            sys.stdout.flush()
             sys.stdout.write("   |")
             # You'll need to choose where to include newlines now.
             sys.stdout.flush()
