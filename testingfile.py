87eddd05873fcc7d4d82574f8f3f15154ad2b051
Karl Fogel <kfogel@red-bean.com>
2023-03-05 14:24:50 -0600

    Show the loopy way of printing a board

--- testingfile.py
+++ testingfile.py
@@ -1,10 +1,15 @@
 import sys
 
-def print_ttt_board():
-    print ("   |   |  ")
-    print ("-----------")
-    print ("   |   |  ")
-    print ("-----------")
-    print ("   |   |  ")
+def print_ttt_board(x, y):
+    """Print a tic-tac-toe board with X columns and Y rows."""
+    i = 0
+    j = 0
+    while i < x:
+        while j < y:
+            # The code from this point on isn't quite correct.
+            print ("   |")  
+            print ("----")
+            j = j + 1
+        i = i + 1
 
-print_ttt_board()
+print_ttt_board(3, 3)
