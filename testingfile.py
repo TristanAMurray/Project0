312182770b35068ad2e49028d98d2654cef7b32e
TristanAMurray <122760620+TristanAMurray@users.noreply.github.com>
2023-03-23 20:39:39 -0500

    Wrote the code that takes the user input.

--- testingfile.py
+++ testingfile.py
@@ -1,26 +1,40 @@
 import sys
 
+def board_size_input(prompt):
+    """Take user input for size of the board."""
+    input_size = input(prompt)
+    try:
+        return int(input_size)
+    except:
+        print("ERROR: I need a number, nothing else.")
+        sys.exit(1)
+
+sizeinput = board_size_input("How wide should the board be?")
+sizeinput2 = board_size_input("How long should the board be?")
+
 def write_now(msg):
     """Write MSG to stdout, and flush immediately."""
     sys.stdout.write(msg)
     sys.stdout.flush()
 
-def print_ttt_board(x, y):
+def print_ttt_board(size1, size2):
     """Print a tic-tac-toe board with X columns and Y rows."""
     i = 0
     j = 0
-    while i < x:
+    size1 = sizeinput
+    size2 = sizeinput2
+    while i < size1:
         # The code inside these loops isn't quite correct yet.
         write_now(f"DEBUG: i is {i}\n")
-        while j < y:
+        while j < size2:
             write_now(f"DEBUG: j is {j}\n")
-            write_now("__")
+            write_now("---")
             # You'll need to choose where to include newlines now.
             write_now("|")
-            write_now("__")
+            write_now("---")
             write_now("|")
-            write_now("__")
+            write_now("---")
             j = j + 1
         i = i + 1
 
-print_ttt_board(3, 3)
+print_ttt_board(3,3)
