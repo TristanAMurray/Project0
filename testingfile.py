da9c6a8504de30b656b3a5d3c51f0e7230f11c0e
Karl Fogel <kfogel@red-bean.com>
2023-03-23 21:13:16 -0500

    Add DEBUG flag and debug_now() function

--- testingfile.py
+++ testingfile.py
@@ -1,5 +1,7 @@
 import sys
 
+DEBUG = False
+
 def board_size_input(prompt):
     """Take user input for size of the board."""
     input_size = input(prompt)
@@ -17,16 +19,23 @@ def write_now(msg):
     sys.stdout.write(msg)
     sys.stdout.flush()
 
+def debug_now(msg):
+    """Iff global DEBUG flag is not False, write MSG to stdout
+    with "DEBUG: " prefix, and flush immediately."""
+    if DEBUG:
+        sys.stdout.write("DEBUG: ")
+        write_now(msg)
+
 def print_ttt_board(size1, size2):
     """Print a tic-tac-toe board with X columns and Y rows."""
     i = 0
    
     while i < size1:
         # The code inside these loops isn't quite correct yet.
-        write_now(f"DEBUG: i is {i}\n")
+        debug_now(f"i is {i}\n")
         j = 0
         while j < size2:
-            write_now(f"DEBUG: j is {j}\n")
+            debug_now(f"j is {j}\n")
             write_now("---")
             # You'll need to choose where to include newlines now.
             write_now("|")
